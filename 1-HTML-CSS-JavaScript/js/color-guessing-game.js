5fe001e4a03a94ac3b4a98e4bd44b4afd144313b
Stephanie Wynn <stephaniewynn@Stephanies-MacBook-Pro.local>
2020-10-13 21:23:42 -0700

    update

---
+++ 1-HTML-CSS-JavaScript/js/color-guessing-game.js
@@ -0,0 +1,14 @@
+const COLORS_ARRAY = ['blue', 'cyan', 'gold', 'gray', 'green', 'magenta', 'orange', 'red', 'white', 'yellow']
+
+function runGame(){
+    let guess = 0;
+    let correct = false;
+
+    const targetIndex = Math.random() * 10;
+    const target = targetIndex
+
+}
+
+function checkGuess(guess,target){
+
+}
